4f8f716c5dac1dad55468e7e483f6b2ce5fb7920
simonedesordi <simone.desordi@gmail.com>
2015-09-09 23:09:22 +0200

    added Dockerfile

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var auth = express.basicAuth(function(user, pass, callback) {
 });
 
 app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 8080);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
     app.use(express.favicon());
